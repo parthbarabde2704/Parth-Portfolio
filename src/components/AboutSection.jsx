daade363ad6b035e86a92411b6d3101fe3431d59
Parth Barabde <parthbarabde2704@gmail.com>
2025-12-16 16:17:55 +0530

    Intial

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,7 @@ export const AboutSection = () => {
   </a>
 
   <a
-    href="/ParthBarabde.pdf"
+    href="/ParthBarabde27.pdf"
     download
     className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
   >
